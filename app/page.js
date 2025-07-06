224c089e66bd15728c0afc7bad0d869ad6506011
GGARJUN <techarjung@gamil.com>
2025-07-06 13:37:11 +0530

    chnage code

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div>
-      Hello, World!
-      Hello, World!
-      Hello, World!
-      Hello, World!
-      Hello, World!
-      Hello, World!
+    <div className="">
+      <h1 className="text-4xl font-bold mb-6">Welcome to My App</h1>
     </div>
   );
 }
